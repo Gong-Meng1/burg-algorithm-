5281e38df840c63625068c3c45fc06310fc43ec7
Mark M. Span <m.m.span@gmail.com>
2021-08-18 22:07:52 +0200

    toolbox

--- @WorkSpace/CreateTreeComponent.m
+++ @WorkSpace/CreateTreeComponent.m
@@ -1,8 +1,10 @@
 function CreateTreeComponent(this)
 % using a very slightly modified tree component, hence the copy in +uiextra
 % the panel is the databrowsert
+    
+    this.Panel = javaObjectEDT('javax.swing.JPanel',javaObjectEDT('java.awt.BorderLayout'));   
+    
     Root = figure('Visible', 'off');
-    this.Panel = javaObjectEDT('javax.swing.JPanel',javaObjectEDT('java.awt.BorderLayout'));    
     this.Tree = uiextras.jTree.Tree('DndEnabled', true, ...
         'Editable', true, ...
         'Parent', Root, ...
@@ -13,13 +15,24 @@ function CreateTreeComponent(this)
     );
     
     %%
-    this.Dummy = javaObjectEDT('javax.swing.JButton', 'Button 1');
+    this.Dummy = javaObjectEDT('javax.swing.JPanel',javaObjectEDT('java.awt.GridLayout',12,12));
+    for i = 1:(12*12)
+        this.Dummy.add(javaObjectEDT('javax.swing.JButton', 'Center'));
+    end 
+%     
+%     fighandle = figure('Visible', 'off');
+%     this.Dummy = uicontrol(...
+%         'style'   , 'pushbutton',...
+%         'parent'  , fighandle,...
+%         'string'  , 'OK',...
+%         'position', [1,1,100,100]);
 
     %%
     
     this.javaObjects = this.Tree.getJavaObjects();
+    
     this.Panel.add(this.Dummy, 'South')
-    this.Panel.add(this.javaObjects.jScrollPane, 'North');
+    this.Panel.add(this.javaObjects.jScrollPane, 'Center');
     
     %% For no obvious reason I put the used icons within "this" class, the Workspace...
     
