2fb11502b6bbd5f7ae632a38358e8f724ec46095
Mark M. Span <m.m.span@gmail.com>
2021-09-09 09:50:18 +0200

    Update pop_getIBI.m

--- Transformations/getIBI/pop_getIBI.m
+++ Transformations/getIBI/pop_getIBI.m
@@ -25,9 +25,7 @@ ecgTimestamps = EEGstruct.times;
 %%  default values:
 fSample = EEGstruct.srate;
 MinPeakHeight = median(ecgData)+(2*std(ecgData));
-par.MinPeakDistance = .250; %seconds!
-par.deTrend = false;
-par.Plot = false;
+par.MinPeakDistance = .50; %seconds!
 
 %% Parse the name - value pairs found in varargin
 %------------------------------------------------------------------------------------------
@@ -43,13 +41,6 @@ end
 
 %% convert MinPeakDistance from ms to samples
 MinPeakDistance = par.MinPeakDistance*fSample;
-%% ------------------------------------------------------------------------------------------
-% are we asked to detrend the data? Detrend algorithm in this file (below).
-oData = ecgData;
-if (~par.deTrend)
-   ecgData = deTrend(ecgData);
-   MinPeakHeight = median(ecgData)+(2*std(ecgData));
-end
 
 
 %% Then, first find the (approximate) peaks
@@ -68,49 +59,11 @@ catch ME
     rethrow(ME);
 end
 
+%% TODO add them to the urevent field of the EEG struct
+
+% then this can go
 EEGstruct.RTopTime = ecgTimestamps(locs) + correction;
 EEGstruct.RTopVal = ecgData(locs);
 EEGstruct.ibis = round(diff(EEGstruct.RTopTime),3);
 
-if (par.Plot) % Plot algorithm in this file (below)
-    figure
-    plot(ecgTimestamps,oData)
-    PlotECGWithIBIS(ecgTimestamps,ecgData,locs,EEGstruct.RTopTime)
-end
-end
-%%
-function ecgData = deTrend(ecgData)
-    [p,~,mu] = polyfit((1:numel(ecgData))',ecgData,16);
-    f_y = polyval(p,(1:numel(ecgData))',[],mu);
-    ecgData = ecgData - f_y';        % Detrend data
-end
-%%
-function PlotECGWithIBIS(ecgTimestamps,ecgData,locs,rtops)
-    %% Plot the ECG trace data
-  
-    ibis = round(diff(rtops/1000),3);
-    hold on
-    plot(ecgTimestamps,ecgData)
-    xlim([ecgTimestamps(1) min(ecgTimestamps(1)+10000,ecgTimestamps(end))]);
-    grid on
-    title('(Detrended) ECG Signal (mV)')
-    xlabel('time (sec)')
-    ylabel('Voltage(mV)')
-    
-    %%  PLOT the Triggered R-tops
-    hold on
-    plot(rtops,ecgData(locs),'ro')
-    
-    %% also plot thim at the top of ther plot:
-    ml = max(ylim);
-    nd = (ecgData(locs)*0) + ml;
-    plot(rtops,nd,'rv','MarkerFaceColor','r')
-    
-    %% Plot the IBI value labels in between the top r-top markers:
-    text( double(rtops(1:end-1) + rtops(2:end))/2, ...
-        double(nd(1:end-1))-50,...
-        int2str(ibis(:)*1000),...
-        'FontSize', 8, 'HorizontalAlignment', 'center');
-    hold off
-    legend('Detrended ECG Signal', 'R-Top')    
 end
